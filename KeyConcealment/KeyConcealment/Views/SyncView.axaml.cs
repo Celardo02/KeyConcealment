f59671eb301c6f4be9d5ac453b604032403bc3f4
Celardo02 <matteo.celardo.social@gmail.com>
2024-09-01 20:51:06 +0200

    Empty GUIs added
    Added all needed GUIs (empty yet):
    - Login: needed to handle login of the user
    - Creds: needed to handle credential sets of the user
    - Master: needed to handle master password
    - Sync: needed to handle syncing process with other devices

---
+++ KeyConcealment/KeyConcealment/Views/SyncView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace KeyConcealment.Views;
+
+public partial class SyncView : UserControl
+{
+    public SyncView()
+    {
+        InitializeComponent();
+    }
+}

53e9866894a835b23ce9ec0fe93ca30f658132d6
Celardo02 <matteo.celardo.social@gmail.com>
2024-09-09 21:40:56 +0200

    Login view created
    Created a login view with a text box to insert the master password and a
    login button. All the presentation logic is supporte by the
    loginViewModel

--- KeyConcealment/KeyConcealment/ViewModels/LoginViewModel.cs
+++ KeyConcealment/KeyConcealment/ViewModels/LoginViewModel.cs
@@ -1,8 +1,40 @@
 using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KeyConcealment.Service;
 
 namespace KeyConcealment.ViewModels;
 
-public class LoginViewModel : ViewModelBase
+public partial class LoginViewModel : ViewModelBase
 {
+    #region  attributes
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    private string? _insPwd;
+    private IService _s;
+    #endregion
+
+    #region constructors
+    public LoginViewModel()
+    {
+        this._s = Handler.Instance;
+    }
+
+    public LoginViewModel(IService s)
+    {
+        this._s = s;
+    }
+    #endregion 
+
+    [RelayCommand(CanExecute = nameof(this.IsPasswordInserted))]
+    private void Login()
+    {
+        this._s.Login(InsPwd);
+    }
+
+    private bool IsPasswordInserted()
+    {
+        return !string.IsNullOrEmpty(InsPwd);
+    }
 
 }

bb180bf5a7a754d372eb7a83fbd9fad53d90edda
Celardo02 <matteo.celardo.social@gmail.com>
2024-09-14 12:19:39 +0200

    PerSecMem class implemented
    - Class change: generic types K and V removed
    - Save method changes:
    	* path definition is now inside a dedicated private method,
    	  called DefinePath
    	* added ';' character removal when they aren't followed by data
    - Load method is now implemented. Added paramters that will be filled
      inside the method to restore app data
    - VaultExists method implemented

--- KeyConcealment/KeyConcealment/Pers/PersSecMem.cs
+++ KeyConcealment/KeyConcealment/Pers/PersSecMem.cs
@@ -11,11 +11,11 @@ namespace KeyConcealment.Pers;
 /// writing operations performed on secondary memory, in order to keep data after closing 
 /// the app and restoring them on following launching
 /// </summary>
-public static class PersSecMem<K,V> where V : ICred<K>
+public static class PersSecMem
 {
     #region attributes
     private static ICrypto? cryp;
-    // name of the directory that will contain the vault file 
+    // name of the directory that is going to contain the vault file 
     private const string VAULT_DIR = "KeyConcealment";
     private const string VAULT_NAME = "Vault.enc";
     #endregion
@@ -28,9 +28,9 @@ public static class PersSecMem<K,V> where V : ICred<K>
     /// <param name="creds">list with all credential sets</param>
     /// <param name="oldNonces">list containing all previously used nonce values</param>
     /// <param name="oldSalts">list containing all previously used salt values</param>
-    public static void Save(string key, IMasterPwd masterPwd, List<V> creds, List<string> oldNonces, List<string> oldSalts)
+    public static void Save(string key, IMasterPwd masterPwd, List<ICred<string>> creds, List<string> oldNonces, List<string> oldSalts)
     {
-        // path where vault will be saved
+        // path where vault is going to be saved
         string path;
         string vaultContent = ""; 
         // KDF function salt value used by AES
@@ -40,35 +40,29 @@ public static class PersSecMem<K,V> where V : ICred<K>
         // AES tag
         string tag = "";
 
-        // choosing the path where the vault will be saved depending on the running OS
-        if (OperatingSystem.IsLinux())
-            // Environment.SpecialFolder.LocalApplicationData translates to /$HOME/.local/share
-            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), VAULT_DIR, VAULT_NAME);
-        else if (OperatingSystem.IsAndroid())
-            // Environment.SpecialFolder.Personal translates to /data/data/[packageName]/files, 
-            // which a dedicated directory for the app on android systems
-            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), VAULT_NAME);
-        else if (OperatingSystem.IsWindows())
-            // Environment.SpecialFolder.ApplicationData translates to AppData dir
-            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), VAULT_DIR, VAULT_NAME);
-        else 
-            throw new PlatformNotSupportedException("Current platform is not supported. KeyConcealment supports only Linux, Windows and Android platforms");
+        path = DefinePath();
 
         // adding all credential sets 
-        foreach(V c in creds)
+        foreach(ICred<string> c in creds)
             vaultContent += c.ToString() + ";";
 
+        // removing last ';' character
+        vaultContent = vaultContent.Remove(vaultContent.Length - 1);
         vaultContent += "\n";
 
         // adding all nonces 
         foreach(string n in oldNonces)
             vaultContent += n + ";";
         
+        // removing last ';' character
+        vaultContent = vaultContent.Remove(vaultContent.Length - 1);
         vaultContent += "\n";
 
         // adding all salts
         foreach(string s in oldSalts)
             vaultContent += s + ";";
+        // removing last ';' character
+        vaultContent = vaultContent.Remove(vaultContent.Length - 1);
 
         cryp = Crypto.Instance;
 
@@ -84,37 +78,106 @@ public static class PersSecMem<K,V> where V : ICred<K>
         File.WriteAllText(path,vaultContent);
     }
 
+    
     /// <summary>
-    /// Load persistences content from vault file
+    /// Loads all the data contained in the vault file using the encryption key
     /// </summary>
-    /// <param name="key">encryption key to use for the file content decryption</param>
-    /// <returns>
-    /// Returns a string containig the content of the file read
-    /// </returns>
-    public static string Load(string key)
+    /// <param name="key">Plain text vault encryption key</param>
+    /// <param name="masterPwd">parameter that is going to be filled with master password data</param>
+    /// <param name="creds">parameter that is going to be filled with credential sets data</param>
+    /// <param name="oldNonces">parameter that is going to be filled with old nonces values</param>
+    /// <param name="oldSalts">parameter that is going to be filled with old salts values</param>
+    public static void Load(string key, ref IMasterPwd? masterPwd, ref List<ICred<string>>? creds, ref List<string>? oldNonces, ref List<string>? oldSalts)
     {
-        /* Reminder: 
-        *  - keep in mind that the password file must contain all the data
-        *  about the master password (the ones in the domain class), all the old 
-        *  values used as nonces or salts and all credential sets.
-        *
-        *  - KDF salt, AES nonce (IV), AES tag and masterPwd hash are saved unencrypted
-        *
-        *  - key must be checked against masterPwd hash to verify if it is correct or 
-        *    not
-        */
-        throw new NotImplementedException();
+        string vaultContent;
+        string mstPwdString;
+        string AESparams;
+        string credsString;
+        string oldNoncesString;
+        string oldSaltsString;
+
+        // checking if the vault exists
+        if(!ValutExists())
+            throw new PersExcNotFound("Vault loading operations failed: vault file does not exist yet.");
+
+        cryp = Crypto.Instance;
+        // importing  vault file
+        vaultContent = File.ReadAllText(DefinePath());
+
+        // gaining all the master password data from vaultContent
+        mstPwdString = vaultContent.Split("\n")[0];
+
+        // checking if key contains the correct value for the master password
+        if(!cryp.VerifyHash(key, mstPwdString.Split(",")[0], mstPwdString.Split(",")[1]))
+            throw new PersExc("Provided encryption key is incorrect");
+
+        // gaining AES salt, nonce and tag from vaultContent
+        AESparams = vaultContent.Split("\n")[1];
+
+        // removing master password data and AES parameters from vaultContent
+        vaultContent = vaultContent.Split("\n")[2];
+        vaultContent = cryp.DecryptAES_GMC(vaultContent, key, AESparams.Split(",")[0], AESparams.Split(",")[1], AESparams.Split(",")[2]);
+
+        // gaining all the vault data
+        credsString = vaultContent.Split("\n")[0];
+        oldNoncesString = vaultContent.Split("\n")[1];
+        oldSaltsString = vaultContent.Split("\n")[2];
+
+        creds = new List<ICred<string>>();
+        oldNonces = new List<string>();
+        oldSalts = new List<string>();
+
+        // loading into method params vault data
+        foreach(string c in credsString.Split(";"))
+            creds.Add(new Credentials(c.Split(",")[0], c.Split(",")[1], c.Split(",")[2], c.Split(",")[3], c.Split(",")[4], DateTime.Parse(c.Split(",")[5]) , c.Split(",")[6], c.Split(",")[7]));
+        
+        foreach(string n in oldNoncesString.Split(";"))
+            oldNonces.Add(n);
+
+        foreach(string s in oldSaltsString.Split(";"))
+            oldSalts.Add(s);
+
+        masterPwd = new MasterPwd(mstPwdString.Split(",")[0],mstPwdString.Split(",")[1],DateTime.Parse(mstPwdString.Split(",")[2]));
     }
 
     /// <summary>
-    /// Checks if a vault exists at a specified path
+    /// Checks whether a vault file exists or not
     /// </summary>
-    /// <param name="path">path where the vault may be found</param>
     /// <returns>
     /// <c>True</c> if the vault exists; <c>False</c> otherwise
     /// </returns>
-    public static bool ValutExists(string path)
+    public static bool ValutExists()
     {        
-        throw new NotImplementedException();
+        return File.Exists(DefinePath());
+    }
+
+    /// <summary>
+    /// Defines vault file path depending on running Operating System
+    /// </summary>
+    /// <returns>
+    /// Returns a path according to the running OS file system standard
+    /// </returns>
+    /// <exception cref="PlatformNotSupportedException">
+    /// Throws a <c>PlatformNotSupportedException</c> if the OS isn't one between 
+    /// Linux, Windows or Android
+    /// </exception>
+    private static string DefinePath()
+    {
+        string path;
+        // choosing the path where the vault is going to be saved depending on the running OS
+        if (OperatingSystem.IsLinux())
+            // Environment.SpecialFolder.LocalApplicationData translates to /$HOME/.local/share
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), VAULT_DIR, VAULT_NAME);
+        else if (OperatingSystem.IsAndroid())
+            // Environment.SpecialFolder.Personal translates to /data/data/[packageName]/files, 
+            // which a dedicated directory for the app on android systems
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), VAULT_NAME);
+        else if (OperatingSystem.IsWindows())
+            // Environment.SpecialFolder.ApplicationData translates to AppData dir
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), VAULT_DIR, VAULT_NAME);
+        else 
+            throw new PlatformNotSupportedException("Current platform is not supported. KeyConcealment supports only Linux, Windows and Android platforms");
+
+        return path;
     }
 }
